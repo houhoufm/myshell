ac3212c10e04a88f27bc011823fc346b9fca3661
hou <245027231@qq.com>
2026-01-04 23:00:17 +0800

    OK

--- test.sh
+++ test.sh
@@ -3,3 +3,16 @@ apt install -y curl
 apt install -y wget
 date
 time
+
+
+
+#curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+#sudo apt-get install git-lfs
+#git lfs install
+#git lfs pull
+#git clone https://github.com/abhishek-raj-2001/ML-Project.git
+#cd ML-Project
+#git lfs pull
+#git checkout main
+#pip install -r requirements.txt
+#python3 main.py
